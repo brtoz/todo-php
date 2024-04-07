b6e0b51fb1d612614047cbd2f1314c0c7a09a112
Berat <beratozfidan@hotmail.com>
2024-04-08 02:43:58 +0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+const BASEDIR = 'C:\xampp\htdocs\todoapp';
+const URL = 'http://localhost/todoapp/';
+const DEV_MODE = true;
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=todoapp;', 'root', '');
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
